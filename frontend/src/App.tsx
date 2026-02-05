27a1088e0c200952db055028f331734d8f35e22c
itushh <tusharramgirkar99@gmail.com>
2026-02-05 18:54:04 +0530

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Lander from "./pages/Lander";
+
+export function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Lander />} />
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Lander.tsx
@@ -0,0 +1,7 @@
+const Lander = () => {
+  return (
+    <div>Lander</div>
+  )
+}
+
+export default Lander
